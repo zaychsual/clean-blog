6d35ac8c09717aa14fe8cde0040ba586b68995f0
zaychsual <zaychsual@gmail.com>
2023-09-23 11:39:26 +0700

    implementasi

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const app = express()
 app.set('views', './src/views')
 app.set('view engine', 'ejs')
 
+//set static public
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/img', express.static(__dirname + 'public/img'))
+
 //set file routes
 require('./src/routes/home.routes')(app)
 
